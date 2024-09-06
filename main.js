fc199f8b64a9b41d9b4a88405c7f9f5d8585f097
Prathiksha <aprathiksha36@gmail.com>
2024-09-06 14:31:11 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Software Development Enthusiast", "Data Science Student", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
